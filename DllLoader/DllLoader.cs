41d15d4e3d2ddf28ee623486d104e823abbe91d2
bchenSyr <36112405+bchenSyr@users.noreply.github.com>
2018-02-03 14:17:07 -0500

    Add files via upload

---
+++ DllLoader/DllLoader.cs
@@ -0,0 +1,100 @@
+/////////////////////////////////////////////////////////////////////
+// DllLoader.cs - Loading Dynamic Link Libraries in specified location                
+// Author: Beier Chen, bchen22@syr.edu
+// Source: Jim Fawcett
+// Application: Project #2, CSE 681 Fall2017                                       
+// Environment: Windows 10 Education, MacBook                                       
+/////////////////////////////////////////////////////////////////////
+/*
+ * Package Operations:
+ * ===================
+ * Loading test dll for TestHardness.
+ * 
+* Public Interface:
+ * ---------------
+ * LoadingDll() - load libraries from a specified path, 
+ *                          call all methods that have no arguments, using dynamic invocation.
+ *                          
+ * Required Files
+ * --------------------
+ * DllLoader.cs
+ * 
+ * Maintenance History:
+ * --------------------
+ * ver 1.0 : Oct 8 2017
+ * - first release
+ * 
+ */
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Federation
+{
+  public class DllLoader
+  {
+        //---< load libraries from a specified path
+        public string LoadingDll(string dllName)
+        {
+            dllName = dllName + ".dll";
+            string path = TestHardnessEnvironment.root;
+            string[] Libraries = Directory.GetFiles(path, dllName);
+            bool testReturn = false;
+            string result;
+            foreach (string library in Libraries)
+            {
+                Console.WriteLine("\n Lib is {0}", library); 
+                Assembly assem = Assembly.LoadFrom(library);
+                Type[] types = assem.GetExportedTypes();
+
+                foreach (Type t in types)
+                {
+                    if (t.IsClass && !t.IsAbstract)
+                    {
+                        object obj = Activator.CreateInstance(t);
+                        MethodInfo[] mis = t.GetMethods();
+                        foreach (MethodInfo mi in mis)
+                        {
+                            if (mi.DeclaringType != typeof(object)) // don't call base members
+                            {
+                                // don't call if method has arguments
+                                try
+                                {
+                                    if (mi.GetParameters().Length == 0/* && mi.Name != "create"*/)
+                                    {
+                                        const BindingFlags bf =
+                                          BindingFlags.Public |
+                                          BindingFlags.InvokeMethod |
+                                          BindingFlags.Instance;
+                                        testReturn = (bool)t.InvokeMember(mi.Name, bf, null, obj, null);
+                                    }
+                                }
+                                catch { /* continue on error - calling factory function create throws */ }
+                            }
+                        }
+                    }
+                }
+            }
+            if (testReturn)
+            {
+                result = "pass";
+            }
+            else result = "fail";      
+            return result;
+        }
+  }
+
+#if(TEST_DLLLOADER)
+    class Test_DllLoader
+    {
+        static void Main()
+        {
+            DllLoader loader = new DllLoader();
+            loader.LoadingDll("CSTestDemo");
+        }
+    }
+#endif
+}
---
+++ Environment/Environment.cs
@@ -0,0 +1,59 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Environment.cs - define environment for components of federation
+// Author: Beier Chen, bchen22@syr.edu
+// Application: Project #4, CSE 681 Fall2017                                  
+// Environment: Windows 10 Education, MacBook               
+/////////////////////////////////////////////////////////////////////
+/*
+ * Maintenance History:
+ * --------------------
+ * ver 1.0 : Dec 8 2017
+ * - first release
+ * 
+ */
+
+namespace Federation
+{
+    public struct Environment
+    {
+        public static string root { get; set; }
+        public static string endPoint { get; set; }
+        public static string address { get; set; }
+        public static int port { get; set; }
+        public static bool verbose { get; set; }
+    }
+
+    public struct ClientEnvironment
+    {
+        public static string root { get; set; } = "../../../StorageOfClient";
+        public static string endPoint { get; set; } = "http://localhost:8080/IMessagePassingComm";
+        public static string baseAdd { get; set; } = "http://localhost";
+        public static int port { get; set; } = 8080;
+    }
+
+    public struct RepoServerEnvironment
+    {
+        public static string root { get; set; } = "../../../StorageOfRepo/";
+        public static string TestDriverDir { get; set; } = "../../../StorageOfRepo/TestDriver";
+        public static string TestDir { get; set; } = "../../../StorageOfRepo/Test";
+        public static string endPoint { get; set; } = "http://localhost:8081/IMessagePassingComm";
+        public static string baseAdd { get; set; } = "http://localhost";
+        public static int port { get; set; } = 8081;
+    }
+
+    public struct BuildServerEnvironment
+    {
+        public static string root { get; set; } = "../../../StorageOfChildBuilder/";
+        public static string endPoint { get; set; } = "http://localhost:8082/IMessagePassingComm";
+        public static string baseAdd { get; set; } = "http://localhost";
+        public static int port { get; set; } = 8082;
+    }
+
+    public struct TestHardnessEnvironment
+    {
+        public static string root { get; set; } = "../../../StorageOfTestHardness";
+        public static string endPoint { get; set; } = "http://localhost:8090/IMessagePassingComm";
+        public static string baseAdd { get; set; } = "http://localhost";
+        public static int port { get; set; } = 8090;
+    }
+}
